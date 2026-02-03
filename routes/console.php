bda928fd57a75ddef222c24edffa1267e11eb028
evgenij <evgenijdudarenko2@gmail.com>
2026-02-03 16:47:02 +0300

    add command in cron

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+app(Schedule::class)->command('weather:get-forecast')
+    ->hourly()
+    ->withoutOverlapping();
+app(Schedule::class)->command('telegram:send-forecast-to-client')
+    ->hourly()
+    ->withoutOverlapping();
